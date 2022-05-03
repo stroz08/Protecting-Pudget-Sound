2de53e009e24ec8411ac8e8f92ad48836aaaa9a7
abequette99 <bequea@uw.edu>
2022-05-03 16:49:58 -0700

    Testing fish cards

--- protecting-puget-sound/src/App.js
+++ protecting-puget-sound/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import { NavBar } from './common/NavigationBar'
+import {Cards} from './Cards.js'
 
 function App() {
   return (
     <div className="page-container">
       <NavBar/>
+      <Cards/>
+      <Cards/>
+      <Cards/>
     </div>
   );
 }
--- protecting-puget-sound/src/cards.js
+++ protecting-puget-sound/src/cards.js
@@ -4,8 +4,8 @@ function Cards(props) {
   return (
       <div className='card'>
         <div className='card_body'>
-          <img src={props.image} class='card_img'/>
-          <h2 className='card_title'>{props.name}</h2>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Salmo_salar.jpg/330px-Salmo_salar.jpg' class='card_img'/>
+          <h2 className='card_title'>Salmon</h2>
         </div>
         <button className='card_button'>View Fish</button>
       </div>
@@ -13,3 +13,4 @@ function Cards(props) {
   )
 
 }
+export {Cards}
