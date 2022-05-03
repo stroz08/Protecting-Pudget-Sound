b71b4a05e2439dd8c7310ea4845042c73628dee6
Annie Bequette <62974504+abequette99@users.noreply.github.com>
2022-05-03 13:00:50 -0700

    Add files via upload

---
+++ protecting-puget-sound/src/cards.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Cards(props) {
+  return (
+      <div className='card'>
+        <div className='card_body'>
+          <img src={props.image} class='card_img'/>
+          <h2 className='card_title'>{props.name}</h2>
+        </div>
+        <button className='card_button'>View Fish</button>
+      </div>
+
+  )
+
+}
