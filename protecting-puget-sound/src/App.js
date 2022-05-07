06a4d51171ad45ca39536f0c33fc72f5f40607ad
abequette99 <bequea@uw.edu>
2022-05-07 10:34:47 -0700

    update pages

--- protecting-puget-sound/src/App.js
+++ protecting-puget-sound/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import React from 'react';
 import { NavBar } from './common/NavigationBar'
-import {Cards} from './Cards.js'
 
 function App() {
   return (
     <div className="page-container">
       <NavBar/>
-      <div className='card_container'>
-      <Cards/>
-      <Cards/>
-      <Cards/>
-      </div>
-
     </div>
   );
 }
---
+++ protecting-puget-sound/src/SpeciesPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Cards} from './Cards.js'
+
+function SpeciesPage() {
+    return (
+      <div>
+      <header>
+
+
+      </header>
+        <div className='card_container'>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        </div>
+      </div>
+    );
+}
+
+export { SpeciesPage };
