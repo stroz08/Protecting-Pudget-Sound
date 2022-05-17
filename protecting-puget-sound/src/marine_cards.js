0889e3332d3142af7089b4893668e5c75e41b2ad
abequette99 <bequea@uw.edu>
2022-05-17 12:08:58 -0700

    replace place holders for marine pages

--- protecting-puget-sound/src/marine_cards.js
+++ protecting-puget-sound/src/marine_cards.js
@@ -5,13 +5,13 @@ function Cards(props) {
   return (
       <div className='card'>
         <div className='card_cont'>
-          <img src='https://www.eregulations.com/assets/images/books/wafw/21wafw/Marine_-_Area11.jpg' class='card_img'/>
-            <div className = 'card_title'>              
-              <h1>Marine Areas</h1>
+          <img src= {props.img} className = 'card_img'/>
+            <div className = 'card_title'>
+              <h1>{props.name}</h1>
         </div>
         </div>
         <div className='button'>
-        <Link className='fish' to='/Marine_Details' activeclassname='activeLink'>Marine Area Detail</Link>
+        <Link className='fish' to= '/Marine_Details' state = {{name:props.name}} activeclassname='activeLink'>Checkout Marine Area</Link>
       </div>
     </div>
 
