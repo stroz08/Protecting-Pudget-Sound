900f112277620c60be5ff11a7403973a7da458a5
Spencer Pudists <pudists7@uw.edu>
2022-05-29 20:51:43 -0700

    Left-bound donation links to resolve SP-71

--- protecting-puget-sound/src/getinvolved.js
+++ protecting-puget-sound/src/getinvolved.js
@@ -11,13 +11,13 @@ function Get_Involved(props) {
           Many factors such as habitat loss, river dams and uncertain ocean conditions continue to negatively impact fish stocks, yet there are still important steps that individual anglers can take.
           Below are some of our favorite resources through which every individual can make a difference.
           </p>
-          <h2 className='fish-des'>Petition</h2>
-                <ul className='align'>
+          <h2>Petition</h2>
+                <ul className='alignBullets'>
                     <li><a href= "https://www.change.org/p/washington-department-of-fish-and-wildlife-protect-puget-sound-juvenile-steelhead">Protect Puget Sound Juvenile Steelhead</a></li>
                     <li><a href= "https://www.oursound-oursalmon.org/taking-back-our-sound-petition#taking-back-our-sound-petition2">Take Back our Sound</a></li>
                 </ul>
-            <h2 class = 'fish-des'>Donate</h2>
-            <ul className='align'>
+            <h2>Donate</h2>
+            <ul className='alignBullets'>
                 <li><a href= "https://lltk.org/donate/">Give to Save Salmon</a></li>
                 <li><a href= "https://lltk.org/donations/provide-sustainable-fishing-in-the-san-juans/">Ensure Fishing in the Puget Sound</a></li>
                 <li><a href= "https://www.earthcorps.org/field-initiatives/protect-puget-sound/">Protect Puget Sound</a></li>
